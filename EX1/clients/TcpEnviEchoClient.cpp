baeb978b14a6bc8c047c8492c019725f75308cc3
abdo643 <abou.shehata643@yahoo.de>
2021-12-04 13:52:04 +0100

    added envi files

---
+++ EX1/clients/TcpEnviEchoClient.cpp
@@ -0,0 +1,5 @@
+//
+// Created by abous on 04/12/2021.
+//
+
+#include "TcpEnviEchoClient.hpp"
---
+++ EX1/clients/TcpEnviEchoClient.hpp
@@ -0,0 +1,14 @@
+//
+// Created by abous on 04/12/2021.
+//
+
+#ifndef EX1_TCPENVIECHOCLIENT_HPP
+#define EX1_TCPENVIECHOCLIENT_HPP
+
+
+class TcpEnviEchoClient {
+
+};
+
+
+#endif //EX1_TCPENVIECHOCLIENT_HPP
