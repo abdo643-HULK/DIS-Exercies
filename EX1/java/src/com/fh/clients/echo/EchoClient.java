4546414d52872df9c7d02166070e98e8deedbfea
abdo643 <abou.shehata643@yahoo.de>
2021-12-13 19:52:24 +0100

    added the readme and fixed styling

--- EX1/java/src/com/fh/clients/echo/EchoClient.java
+++ EX1/java/src/com/fh/clients/echo/EchoClient.java
@@ -8,47 +8,47 @@ import java.io.*;
 import java.net.*;
 
 public class EchoClient {
-    Socket mEchoSocket = null;
-    PrintWriter mOut = null;
-    BufferedReader mIn = null;
+	Socket mEchoSocket = null;
+	PrintWriter mOut = null;
+	BufferedReader mIn = null;
 
-    public void setupConnection(int _port, String _serverIP) {
+	public void setupConnection(int _port, String _serverIp) {
 
-        System.out.println("Trying to connect to host: " + _serverIP + ":" + _port);
+		System.out.println("Trying to connect to host: " + _serverIp + ":" + _port);
 
-        try {
-            mEchoSocket = new Socket(_serverIP, _port);
-            mOut = new PrintWriter(mEchoSocket.getOutputStream(), true);
-            mIn = new BufferedReader(new InputStreamReader(mEchoSocket.getInputStream()));
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: " + _serverIP);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for " + "the connection to: " + _serverIP);
-            System.exit(1);
-        }
-    }
+		try {
+			mEchoSocket = new Socket(_serverIp, _port);
+			mOut = new PrintWriter(mEchoSocket.getOutputStream(), true);
+			mIn = new BufferedReader(new InputStreamReader(mEchoSocket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: " + _serverIp);
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for " + "the connection to: " + _serverIp);
+			System.exit(1);
+		}
+	}
 
-    public void startRequest() throws IOException {
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+	public void startRequest() throws IOException {
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-        String userInput;
-        System.out.print("input: ");
-        while ((userInput = stdIn.readLine()) != null) {
-            mOut.println(userInput);
+		String userInput;
+		System.out.print("input: ");
+		while ((userInput = stdIn.readLine()) != null) {
+			mOut.println(userInput);
 
-            if (userInput.equals("shutdown")) {
-                System.out.print("Server shutting down -> Closing client connection: ");
-                break;
-            }
+			if (userInput.equals("shutdown")) {
+				System.out.print("Server shutting down -> Closing client connection: ");
+				break;
+			}
 
-            System.out.println(mIn.readLine());
-            System.out.print("input: ");
-        }
+			System.out.println(mIn.readLine());
+			System.out.print("input: ");
+		}
 
-        mOut.close();
-        mIn.close();
-        stdIn.close();
-        mEchoSocket.close();
-    }
+		mOut.close();
+		mIn.close();
+		stdIn.close();
+		mEchoSocket.close();
+	}
 }
--- EX1/java/src/com/fh/clients/envi/TcpEnviClient.java
+++ EX1/java/src/com/fh/clients/envi/TcpEnviClient.java
@@ -4,180 +4,183 @@
 
 package com.fh.clients.envi;
 
-
 import java.io.*;
 import java.net.*;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
-
+/**
+ * Client for our C++ Server.
+ *
+ * @author Marko Milo
+ */
 public class TcpEnviClient implements IEnvService {
+	Socket mSocket = null;
+	PrintWriter mOut = null;
+	BufferedReader mIn = null;
 
-    Socket mSocket = null;
-    PrintWriter mOut = null;
-    BufferedReader mIn = null;
+	public void setupConnection(int _port, String _serverIp) {
 
-    public void setupConnection(int _port, String _serverIP) {
+		System.out.println("Trying to connect to host: " + _serverIp + ":" + _port);
 
-        System.out.println("Trying to connect to host: " + _serverIP + ":" + _port);
+		try {
+			mSocket = new Socket(_serverIp, _port);
+			mOut = new PrintWriter(mSocket.getOutputStream(), true);
+			mIn = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: " + _serverIp);
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to: " + _serverIp);
+			System.exit(1);
+		}
+	}
 
-        try {
-            mSocket = new Socket(_serverIP, _port);
-            mOut = new PrintWriter(mSocket.getOutputStream(), true);
-            mIn = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: " + _serverIP);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to: " + _serverIP);
-            System.exit(1);
-        }
-    }
+	public void startRequest() throws IOException {
+		Scanner in = new Scanner(System.in);
+		int inputMenu;
+		boolean exit = false;
 
-    public void startRequest() throws IOException {
-        Scanner in = new Scanner(System.in);
-        int inputMenu;
-        boolean exit = false;
+		while (!exit) {
+			do {
+				System.out.println();
+				System.out.println("Envi Client Menu:");
+				System.out.println("------------------------:");
+				System.out.println("1. Get sensor types:");
+				System.out.println("2. Get sensor data:");
+				System.out.println("3. Get all Sensor:");
+				System.out.println("4. EXIT");
+				System.out.println("------------------------:");
 
-        while (!exit) {
-            do {
-                System.out.println();
-                System.out.println("Envi Client Menu:");
-                System.out.println("------------------------:");
-                System.out.println("1. Get sensor types:");
-                System.out.println("2. Get sensor data:");
-                System.out.println("3. Get all Sensor:");
-                System.out.println("4. EXIT");
-                System.out.println("------------------------:");
+				inputMenu = in.nextInt();
 
-                inputMenu = in.nextInt();
+			} while (inputMenu < 1 || inputMenu > 4);
 
-            } while (inputMenu < 1 || inputMenu > 4);
+			switch (inputMenu) {
+				case 1 -> getSensorType();
+				case 2 -> {
+					System.out.print("Please provide the sensor type: ");
+					String input = in.next();
+					getSensor(input);
+				}
+				case 3 -> getAllSensors();
+				default -> exit = true;
+			}
 
-            switch (inputMenu) {
-                case 1 -> getSensorType();
-                case 2 -> {
-                    System.out.print("Please provide the sensor type: ");
-                    String input = in.next();
-                    getSensor(input);
-                }
-                case 3 -> getAllSensors();
-                default -> exit = true;
-            }
+		}
 
-        }
+		mOut.close();
+		in.close();
+		mSocket.close();
+	}
 
-        mOut.close();
-        in.close();
-        mSocket.close();
+	public void getSensorType() {
+		String[] s = requestEnvironmentDataTypes();
 
-    }
+		for (String str : s) {
+			System.out.println(str);
+		}
+	}
 
-    public void getSensorType() {
-        String[] s = requestEnvironmentDataTypes();
+	public void getAllSensors() {
+		EnvData[] s = requestAll();
 
-        for (String str : s) {
-            System.out.println(str);
-        }
+		for (EnvData str : s) {
+			String timestamp = "Timestamp: " + str.mTimeStamp;
+			String sensor = "Sensor: " + str.mValues[0];
+			String value = "Value: " + str.mValues[1];
+			System.out.println(timestamp + ", " + sensor + ", " + value);
+		}
+	}
 
-    }
+	public void getSensor(String _type) {
+		EnvData s = requestEnvironmentData(_type);
 
-    public void getAllSensors() {
-        EnvData[] s = requestAll();
-
-        for (EnvData str : s) {
-            System.out.println("Timestamp: " + str.mTimeStamp + ", Sensor: " + str.mValues[0] + ", Value: " + str.mValues[1]);
-        }
-    }
-
-    public void getSensor(String _type) {
-        EnvData s = requestEnvironmentData(_type);
-
-        for (String str : s.mValues) {
-            System.out.println("Timestamp: " + s.mTimeStamp + ", Value: " + str);
-        }
-    }
+		for (String str : s.mValues) {
+			System.out.println("Timestamp: " + s.mTimeStamp + ", Value: " + str);
+		}
+	}
 
 
-    @Override
-    public String[] requestEnvironmentDataTypes() {
-        String endpoint = "GET_SENSOR_TYPES";
-        String input = null;
+	@Override
+	public String[] requestEnvironmentDataTypes() {
+		String endpoint = "GET_SENSOR_TYPES";
+		String input = null;
 
-        try {
+		try {
 
-            mOut.printf(endpoint);
-            input = mIn.readLine();
+			mOut.printf(endpoint);
+			input = mIn.readLine();
 
-            if (Objects.equals(input, "NOT FOUND")) {
-                System.err.println("ERROR FETCHING ENV DATA");
-                return null;
-            }
+			if (Objects.equals(input, "NOT FOUND")) {
+				System.err.println("ERROR FETCHING ENV DATA");
+				return null;
+			}
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        if (input == null) return null;
-        return input.split(";");
-    }
+		if (input == null) return null;
+		return input.split(";");
+	}
 
-    @Override
-    public EnvData requestEnvironmentData(String _type) {
-        mOut.println(_type);
-        EnvData envData = null;
+	@Override
+	public EnvData requestEnvironmentData(String _type) {
+		mOut.println(_type);
+		EnvData envData = null;
 
-        try {
-            String input = mIn.readLine();
+		try {
+			String input = mIn.readLine();
 
-            if (Objects.equals(input, "NOT FOUND")) {
-                System.err.println("ERROR FETCHING ENV DATA");
-                return null;
-            }
+			if (Objects.equals(input, "NOT FOUND")) {
+				System.err.println("ERROR FETCHING ENV DATA");
+				return null;
+			}
 
-            String[] s = input.split("\\|");
-            String timestamp = s[0];
-            String data = s[1];
+			String[] s = input.split("\\|");
+			String timestamp = s[0];
+			String data = s[1];
 
-            envData = new EnvData(timestamp, data.split(";"));
+			envData = new EnvData(timestamp, data.split(";"));
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        return envData;
-    }
+		return envData;
+	}
 
-    @Override
-    public EnvData[] requestAll() {
-        String endpoint = "GET_ALL";
-        mOut.println(endpoint);
+	@Override
+	public EnvData[] requestAll() {
+		String endpoint = "GET_ALL";
+		mOut.println(endpoint);
 
-        String input;
-        EnvData[] envData = new EnvData[0];
+		String input;
+		EnvData[] envData = new EnvData[0];
 
-        try {
-            input = mIn.readLine();
-            String[] s = input.split("\\|");
+		try {
+			input = mIn.readLine();
+			String[] s = input.split("\\|");
 
-            if (Objects.equals(input, "NOT FOUND")) {
-                System.err.println("ERROR FETCHING ENV DATA");
-                return null;
-            }
+			if (Objects.equals(input, "NOT FOUND")) {
+				System.err.println("ERROR FETCHING ENV DATA");
+				return null;
+			}
 
-            String timestamp = s[0];
-            String[] data = Arrays.copyOfRange(s, 1, s.length);
-            envData = new EnvData[data.length];
+			String timestamp = s[0];
+			String[] data = Arrays.copyOfRange(s, 1, s.length);
+			envData = new EnvData[data.length];
 
-            for (int i = 0; i < data.length; i++) {
-                envData[i] = new EnvData(timestamp, data[i].split(";"));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			for (int i = 0; i < data.length; i++) {
+				envData[i] = new EnvData(timestamp, data[i].split(";"));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        return envData;
+		return envData;
 
-    }
+	}
 }
