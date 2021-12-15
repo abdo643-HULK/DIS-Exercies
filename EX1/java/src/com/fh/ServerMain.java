e7bfb9c8041b50af5e010cb8cefe82ae7f43616e
abdo643 <abou.shehata643@yahoo.de>
2021-12-15 19:09:57 +0100

    naming fixed

--- EX1/java/src/com/fh/ServerMain.java
+++ EX1/java/src/com/fh/ServerMain.java
@@ -12,20 +12,19 @@ import java.io.*;
  * The ServerMain simply contains a main method
  * where the server side will be executed.
  */
-public class ServerMain
-{
-    /**
-     * main method for executing Client side.
-     *
-     * @param _args contains the server IP address and server port
-     * @throws IOException
-     */
-    public static void main(String[] _args) throws IOException
-    {
-        int _port = 3001;
+public class ServerMain {
+	/**
+	 * main method for executing Client side.
+	 *
+	 * @param _args contains the server IP address and server port
+	 * @throws IOException
+	 *
+	 */
+	public static void main(String[] _args) throws IOException {
+		int port = 3001;
 
-        EchoServer echoServer = new EchoServer();
-        echoServer.initializeSocket(_port);
-        echoServer.communication();
-    }
+		EchoServer echoServer = new EchoServer();
+		echoServer.initializeSocket(port);
+		echoServer.communication();
+	}
 }
